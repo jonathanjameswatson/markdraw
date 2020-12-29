7aae2d943c74a63376411e319c2c82ca4275cc34
Jonathan Watson <jonathanw12@outlook.com>
2020-12-29 17:27:23 +0000

    Remove struckthrough and underlined

--- Markdraw.Delta/TextFormat.cs
+++ Markdraw.Delta/TextFormat.cs
@@ -5,16 +5,12 @@ namespace Markdraw.Delta
     public bool Bold = false;
     public bool Italic = false;
     public string Link = "";
-    public bool Underlined = false;
-    public bool Struckthrough = false;
 
     public TextFormat(bool bold, bool italic, string link, bool underlined, bool struckthrough)
     {
       Bold = bold;
       Italic = italic;
       Link = link;
-      Underlined = underlined;
-      Struckthrough = struckthrough;
     }
 
     public TextFormat() { }
@@ -24,8 +20,6 @@ namespace Markdraw.Delta
       return (Bold == textFormat.Bold
               && Italic == textFormat.Italic
               && Link == textFormat.Link
-              && Underlined == textFormat.Underlined
-              && Struckthrough == textFormat.Struckthrough
              );
     }
   }
