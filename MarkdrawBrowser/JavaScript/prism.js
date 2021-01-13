6e34f1813d281a8a3c9de6824ca19212d7d1eeff
Jonathan Watson <jonathanw12@outlook.com>
2021-01-13 21:41:09 +0000

    Set languages path to be absolute

--- MarkdrawBrowser/JavaScript/prism.js
+++ MarkdrawBrowser/JavaScript/prism.js
@@ -2,7 +2,7 @@ import Prism from 'prismjs';
 import 'prismjs/plugins/autoloader/prism-autoloader.min.js';
 import 'prismjs/plugins/custom-class/prism-custom-class.min.js';
 
-Prism.plugins.autoloader.languages_path = 'grammars/';
+Prism.plugins.autoloader.languages_path = 'https://jonathanjameswatson.com/markdraw/grammars/';
 Prism.plugins.customClass.prefix('prism--');
 
 export default () => Prism.highlightAll();
