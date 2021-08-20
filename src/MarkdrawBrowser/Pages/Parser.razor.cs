ac93df121ba390028c0eaf54fc331a357e8610cb
Jonathan Watson <jonathanw12@outlook.com>
2021-08-20 16:05:25 +0100

    Add HTML parser page

---
+++ src/MarkdrawBrowser/Pages/Parser.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MarkdrawBrowser.Pages
+{
+  public partial class Parser : ComponentBase
+  {
+    private const string Original = "# Parser\n\nUse this page to see how Markdraw handles Markdown.";
+
+    private string _input = Original;
+    private string _htmlOutput = GetHtml(Original);
+
+    private string Input
+    {
+      get => _input;
+      set
+      {
+        _input = value;
+        _htmlOutput = GetHtml(value);
+      }
+    }
+
+    private static string GetHtml(string input)
+    {
+      return Markdraw.Parser.Parser.Parse(input);
+    }
+  }
+}
