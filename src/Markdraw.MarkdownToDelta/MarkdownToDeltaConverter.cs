75bd25cc0ccdc75fb5951c30f298bc952096a001
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2021-12-14 15:45:57 +0000

    Make MarkdownToDeltaConverter nullable

--- src/Markdraw.MarkdownToDelta/MarkdownToDeltaConverter.cs
+++ src/Markdraw.MarkdownToDelta/MarkdownToDeltaConverter.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Immutable;
-using System.Diagnostics;
 using Markdig;
 using Markdig.Syntax;
 using Markdig.Syntax.Inlines;
@@ -24,10 +23,10 @@ public static class MarkdownToDeltaConverter
   }
 
   private static void Write(Document document, IMarkdownObject block,
-    ImmutableList<IEnumerator<Indent>> indentSequences, IEnumerator<ListIndent> listSequence = null)
+    ImmutableList<IEnumerator<Indent>> indentSequences, IEnumerator<ListIndent>? listSequence = null)
   {
     var newIndentSequences = indentSequences;
-    IEnumerator<ListIndent> newListSequence = null;
+    IEnumerator<ListIndent>? newListSequence = null;
 
     switch (block)
     {
@@ -38,12 +37,10 @@ public static class MarkdownToDeltaConverter
         {
           case LinkReferenceDefinitionGroup:
             return;
-          case ListBlock listBlock:
-            var loose = listBlock.IsLoose;
-            if (listBlock.IsOrdered)
+          case ListBlock { IsLoose: var loose, IsOrdered: var ordered, OrderedStart: var orderedStart }:
+            if (ordered)
             {
-              Debug.Assert(listBlock.OrderedStart != null, "listBlock.OrderedStart != null");
-              var newIndent = Indent.Number(int.Parse(listBlock.OrderedStart), loose);
+              var newIndent = Indent.Number(int.Parse(orderedStart ?? "1"), loose);
               newListSequence = SequenceHelpers.RepeatAfterFirst<ListIndent>(newIndent, newIndent with {
                 Start = 0
               });
@@ -56,13 +53,15 @@ public static class MarkdownToDeltaConverter
               });
             }
             break;
-          case ListItemBlock:
-            Debug.Assert(listSequence != null, nameof(listSequence) + " != null");
+          case ListItemBlock when listSequence is not null:
             listSequence.MoveNext();
             newIndentSequences =
               newIndentSequences.Add(SequenceHelpers.RepeatAfterFirst<Indent>(listSequence.Current,
                 listSequence.Current.NextIndent));
             break;
+          case ListItemBlock:
+            throw new InvalidOperationException(
+              "A list item block should not be read while there is no list sequence.");
           case MarkdownDocument:
             break;
           case QuoteBlock:
@@ -81,11 +80,11 @@ public static class MarkdownToDeltaConverter
             Indents = SequenceHelpers.YieldFromSequences(newIndentSequences).ToImmutableList(), Header = 0
           }));
         }
+
         foreach (var child in containerBlock)
         {
           Write(document, child, newIndentSequences, newListSequence);
         }
-
 
         break;
       case LeafBlock leafBlock:
@@ -96,23 +95,29 @@ public static class MarkdownToDeltaConverter
         {
           case EmptyBlock:
             return;
-          case FencedCodeBlock fencedCodeBlock:
-            document.Insert(new CodeInsert(fencedCodeBlock.Lines.ToString(), fencedCodeBlock.Info));
+          case FencedCodeBlock { Lines: var lines, Info: var info }:
+            document.Insert(new CodeInsert(lines.ToString(), info ?? ""));
             break;
-          case CodeBlock codeBlock:
-            document.Insert(new CodeInsert(codeBlock.Lines.ToString()));
+          case CodeBlock { Lines: var lines }:
+            document.Insert(new CodeInsert(lines.ToString()));
             break;
-          case HeadingBlock headingBlock:
-            InsertInline(document, headingBlock.Inline);
-            header = headingBlock.Level;
+          case HeadingBlock { Inline: Inline inline, Level: var level }:
+            InsertInline(document, inline);
+            header = level;
             break;
-          case HtmlBlock htmlBlock:
-            document.Insert(new BlockHtmlInsert(htmlBlock.Lines.ToString()));
+          case HeadingBlock { Level: var level }:
+            document.Insert(new LineInsert(new LineFormat(indents, level)));
+            break;
+          case HtmlBlock { Lines: var lines }:
+            document.Insert(new BlockHtmlInsert(lines.ToString()));
             break;
           case LinkReferenceDefinition:
             return;
-          case ParagraphBlock paragraphBlock:
-            InsertInline(document, paragraphBlock.Inline);
+          case ParagraphBlock { Inline: Inline inline }:
+            InsertInline(document, inline);
+            break;
+          case ParagraphBlock:
+            document.Insert(new LineInsert(new LineFormat(indents)));
             break;
           case ThematicBreakBlock:
             document.Insert(new DividerInsert());
@@ -129,9 +134,10 @@ public static class MarkdownToDeltaConverter
     }
   }
 
-  private static void InsertInline(Document document, Inline inline, InlineFormat format = null)
+  private static void InsertInline(Document document, Inline inline, InlineFormat? format = null)
   {
     var newFormat = format ?? new InlineFormat();
+
     switch (inline)
     {
       case ContainerInline containerInline:
@@ -141,28 +147,23 @@ public static class MarkdownToDeltaConverter
             var literal = delimiterInline.ToLiteral();
             document.Insert(new TextInsert(literal, newFormat));
             break;
-          case EmphasisInline emphasisInline:
-            newFormat = emphasisInline.DelimiterCount switch {
-              (< 2) => newFormat with {
-                Styles = newFormat.Styles.Add(Style.Italic)
-              },
-              _ => newFormat with {
-                Styles = newFormat.Styles.Add(Style.Bold)
-              }
+          case EmphasisInline { DelimiterCount: < 2 }:
+            newFormat = newFormat with {
+              Styles = newFormat.Styles.Add(Style.Italic)
             };
             break;
-          case LinkInline linkInline:
-            if (linkInline.IsImage)
-            {
-              var altText = GetText(linkInline);
-              document.Insert(new ImageInsert(linkInline.Url, altText, linkInline.Title ?? "") with {
-                Format = newFormat
-              });
-              return;
-            }
-
+          case EmphasisInline:
             newFormat = newFormat with {
-              Styles = newFormat.Styles.Add(Style.Link(linkInline.Url, linkInline.Title ?? ""))
+              Styles = newFormat.Styles.Add(Style.Bold)
+            };
+            break;
+          case LinkInline { IsImage: true, Url: var url, Title: var title } linkInline:
+            var altText = GetText(linkInline);
+            document.Insert(new ImageInsert(url ?? "", altText, title ?? "", newFormat));
+            return;
+          case LinkInline { Url: var url, Title: var title }:
+            newFormat = newFormat with {
+              Styles = newFormat.Styles.Add(Style.Link(url ?? "", title ?? ""))
             };
             break;
         }
@@ -171,21 +172,23 @@ public static class MarkdownToDeltaConverter
         {
           InsertInline(document, child, newFormat);
         }
+
         break;
       case LeafInline leafInline:
         InlineInsert newInsert = leafInline switch {
-          AutolinkInline autolinkInline => new TextInsert(autolinkInline.Url),
-          CodeInline codeInline => new TextInsert(codeInline.Content),
-          HtmlEntityInline htmlEntityInline => new InlineHtmlInsert(htmlEntityInline.Original.ToString()),
-          HtmlInline htmlInline => new InlineHtmlInsert(htmlInline.Tag),
-          LineBreakInline { IsHard: var hard } => hard ? new InlineHtmlInsert("<br />") : new TextInsert(" "),
-          LiteralInline literalInline => new TextInsert(literalInline.Content.ToString()),
+          AutolinkInline { Url: var url } => new TextInsert(url),
+          CodeInline { Content: var content } => new TextInsert(content),
+          HtmlEntityInline { Original: var original } => new InlineHtmlInsert(original.ToString()),
+          HtmlInline { Tag: var tag } => new InlineHtmlInsert(tag),
+          LineBreakInline { IsHard: true } => new InlineHtmlInsert("<br />"),
+          LineBreakInline => new TextInsert(" "),
+          LiteralInline { Content: var content } => new TextInsert(content.ToString()),
           _ => throw new ArgumentOutOfRangeException(nameof(inline))
         };
 
         newFormat = leafInline switch {
-          AutolinkInline autolinkInline => newFormat with {
-            Styles = newFormat.Styles.Add(Style.Link(autolinkInline.Url))
+          AutolinkInline { Url: var url } => newFormat with {
+            Styles = newFormat.Styles.Add(Style.Link(url))
           },
           CodeInline => newFormat with {
             Code = true
@@ -196,6 +199,7 @@ public static class MarkdownToDeltaConverter
         document.Insert(newInsert with {
           Format = newFormat
         });
+
         break;
       default:
         throw new ArgumentOutOfRangeException(nameof(inline));
