167a5d842bdede71d043de64950cd8b9c46e8cc7
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2021-12-16 23:25:19 +0000

    Yarn upgrade and modify webpack config

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,59 @@
-"use strict";
+'use strict';
 
 /*global require*/
 /*global process*/
 
-const gulp = require("gulp");
+const path = require('path');
+const gulp = require('gulp');
 const changed = require('gulp-changed');
-const sass = require("gulp-sass")(require("node-sass"));
-const webpack_stream = require("webpack-stream");
-const webpack_config = require("./webpack.config.js");
+const sass = require('gulp-sass')(require('node-sass'));
+const webpack_stream = require('webpack-stream');
+const webpack_config = require('./webpack.config.js');
 
 const dirs = {
   scss: {
-    src: "src/MarkdrawBrowser/Styles",
-    dest: "src/MarkdrawBrowser/wwwroot/css",
+    src: './src/MarkdrawBrowser/Styles',
+    dest: './src/MarkdrawBrowser/wwwroot/css',
   },
   js: {
-    src: "src/MarkdrawBrowser/JavaScript",
-    dest: "src/MarkdrawBrowser/wwwroot",
+    src: './src/MarkdrawBrowser/JavaScript',
+    dest: './src/MarkdrawBrowser/wwwroot',
   },
   grammars: {
-    src: "node_modules/prismjs/components",
-    dest: "src/MarkdrawBrowser/wwwroot/grammars",
+    src: './node_modules/prismjs/components',
+    dest: './src/MarkdrawBrowser/wwwroot/grammars',
   },
 };
 
 const buildStyles = () => gulp
     .src(`${dirs.scss.src}/*.scss`)
     .pipe(changed(dirs.scss.dest))
-    .pipe(sass().on("error", sass.logError))
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(dirs.scss.dest));
+
 buildStyles.displayName = 'build-styles';
+exports.buildStyles = buildStyles;
+
 
 const buildScripts = () => webpack_stream(webpack_config).pipe(gulp.dest(dirs.js.dest));
+
 buildScripts.displayName = 'build-scripts';
+exports.buildScripts = buildScripts;
+
 
 const buildGrammars = () => gulp
   .src([`${dirs.grammars.src}/**`])
   .pipe(changed(dirs.grammars.dest))
   .pipe(gulp.dest(dirs.grammars.dest));
+
 buildGrammars.displayName = 'build-grammars';
+exports.buildGrammars = buildGrammars;
+
 
 const build = gulp.parallel(buildStyles, buildScripts, buildGrammars);
+
+exports.build = build;
+
 
 const watch = () => {
   gulp.watch(`${dirs.scss.src}/**/*.scss`, { ignoreInitial: false }, buildStyles);
@@ -48,5 +61,5 @@ const watch = () => {
 };
 
 exports.watch = watch;
-exports.build = build;
-exports.buildGrammars = buildGrammars;
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,23 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  resolve: {
-    extensions: [".js"],
-  },
-  mode: process.env.NODE_ENV ?? "development",
-  devtool: process.env.NODE_ENV === "production" ? "none" : "inline-source-map",
+  entry: path.resolve(__dirname, './src/MarkdrawBrowser/JavaScript/index.js'),
   module: {
     rules: [
       {
-        test: /\.js?$/,
-        include: path.resolve(__dirname, 'src/MarkdrawBrowser/JavaScript'),
-        loader: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            cacheCompression: false,
-            cacheDirectory: true,
-          },
-        },
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
       },
     ],
   },
-  entry: "./src/MarkdrawBrowser/JavaScript/index.js",
-  output: {
-    path: path.resolve(__dirname, "src/MarkdrawBrowser/wwwroot"),
-    filename: "markdrawBrowser.bundle.js",
+  resolve: {
+    extensions: ['*', '.js'],
   },
+  output: {
+    path: path.resolve(__dirname, './src/MarkdrawBrowser/wwwroot'),
+    filename: 'markdrawBrowser.bundle.js',
+  },
+  mode: process.env.NODE_ENV ?? 'development',
+  devtool: process.env.NODE_ENV === 'production' ? 'none' : 'inline-source-map',
 };
