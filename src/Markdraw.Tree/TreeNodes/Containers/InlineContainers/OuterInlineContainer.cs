ae2a565ad0dfe7c04892ae6007e59c5588f29c25
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2021-12-27 12:46:33 +0000

    Fix empty paragraph tags

--- src/Markdraw.Tree/TreeNodes/Containers/InlineContainers/OuterInlineContainer.cs
+++ src/Markdraw.Tree/TreeNodes/Containers/InlineContainers/OuterInlineContainer.cs
@@ -22,8 +22,9 @@ public class OuterInlineContainer : InlineBranchingContainer
   public bool Loose { get; set; }
 
   protected override string Tag => Header == 0 ? "p" : $"h{Header}";
-  protected override string StartingTag => Loose ? $"<{Tag}>" : "";
-  protected override string EndingTag => Loose ? $"</{Tag}>" : "";
+  private bool ShowTags => Loose && !(Header == 0 && ElementsInside.Count == 0);
+  protected override string StartingTag => ShowTags ? $"<{Tag}>" : "";
+  protected override string EndingTag => ShowTags ? $"</{Tag}>" : "";
 
   public static OuterInlineContainer CreateInstance(int depth, IEnumerable<InlineInsert> document,
     DeltaTree? deltaTree = null, int i = 0, int header = 0, bool loose = false)
