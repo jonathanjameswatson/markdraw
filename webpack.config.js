0bc15ffb147f5a6a982bc39d777c1ed20fea87a5
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2022-06-15 21:45:56 +0100

    Upgrade dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   resolve: {
-    extensions: ['.js']
+    extensions: [".js"],
   },
   mode: process.env.NODE_ENV,
-  devtool: process.env.NODE_ENV === 'production' ? 'none' : 'inline-source-map',
+  devtool: process.env.NODE_ENV === "production" ? "none" : "inline-source-map",
   module: {
     rules: [
       {
         test: /\.js?$/,
-        loader: {
-          loader: 'babel-loader',
+        use: {
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
-    ]
+    ],
   },
-  entry: './MarkdrawBrowser/JavaScript/index.js',
+  entry: "./MarkdrawBrowser/JavaScript/index.js",
   output: {
-    path: path.join(__dirname, '/MarkdrawBrowser/wwwroot'),
-    filename: 'markdrawBrowser.bundle.js'
-  }
+    path: path.join(__dirname, "/MarkdrawBrowser/wwwroot"),
+    filename: "markdrawBrowser.bundle.js",
+  },
 };
